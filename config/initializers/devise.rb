8bce1dfe189225a0f99d72b2d42ea089296e0719
crosbourne <neilcrosbourne@hotmail.com>
2015-02-26 16:17:56 +0000

    edited oauth fb keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :facebook, ENV['FBAccessKeyId'], ENV['FBSecretKey'] 
+   config.omniauth :facebook, ENV['FBAccessKeyId1'], ENV['FBSecretKey1'] 
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
