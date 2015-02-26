372bf00b7bbc830b6ba28668497da9778cf6b702
crosbourne <neilcrosbourne@hotmail.com>
2015-02-26 22:34:51 +0000

    fixed fb invite button

--- app/assets/javascripts/bill.js
+++ app/assets/javascripts/bill.js
@@ -4,3 +4,4 @@ $(function(){
     console.log('submited');
   });
 });
+
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -6,3 +6,15 @@
       console.log("working");
     });
   });
+
+
+  // $(function(){
+  //   $("#invite_fb_friends").click(function(){
+  //     FB.init({
+  //       appId:  'ENV[FBAccessKeyId1]',
+  //       cookie: false,
+  //       status: true
+  //     });
+  //     FB.ui({ method: 'apprequests', message: '<%= current_user %> thinks you are really awesome'});
+  //   });
+  // });
